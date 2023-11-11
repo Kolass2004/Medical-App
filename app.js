836f492bba5536f008c737f8c26161f3f12526d3
Kolass rexon J <89210243+Kolass2004@users.noreply.github.com>
2023-11-11 20:31:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+// Updated JavaScript (app.js)
+
+let userData;
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Your initialization code goes here
+
+    // Load user data from external JSON file
+    fetch('userdata.json')
+        .then(response => response.json())
+        .then(data => {
+            userData = data;
+            createDiseaseCheckboxes(Object.keys(userData.medications));
+        })
+        .catch(error => console.error('Error loading user data:', error));
+});
+
+function createDiseaseCheckboxes(diseases) {
+    const diseaseListDiv = document.getElementById('disease-list');
+
+    diseases.forEach(disease => {
+        const checkboxLabel = document.createElement('label');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `${disease.toLowerCase()}Checkbox`;
+        checkboxLabel.appendChild(checkbox);
+        checkboxLabel.appendChild(document.createTextNode(` ${disease}`));
+
+        diseaseListDiv.appendChild(checkboxLabel);
+        diseaseListDiv.appendChild(document.createElement('br')); // Add line break for better spacing
+    });
+}
+
+function getSelectedMedications() {
+    const selectedDiseases = getSelectedDiseases();
+
+    // Fetch medications for selected diseases
+    const selectedMedications = getSelectedMedicationsFromData(userData.medications, selectedDiseases);
+    renderMedications(selectedMedications);
+}
+
+function getSelectedDiseases() {
+    const selectedDiseases = [];
+    const checkboxes = document.querySelectorAll('#disease-list input[type="checkbox"]:checked');
+
+    checkboxes.forEach(checkbox => {
+        selectedDiseases.push(checkbox.id.replace('Checkbox', '').toLowerCase());
+    });
+
+    return selectedDiseases;
+}
+
+function getSelectedMedicationsFromData(allMedications, selectedDiseases) {
+    const selectedMedications = {};
+
+    selectedDiseases.forEach(disease => {
+        selectedMedications[disease] = allMedications[disease];
+    });
+
+    return selectedMedications;
+}
+
+function renderMedications(medications) {
+    const medicationsList = document.getElementById('medications-list');
+    medicationsList.innerHTML = '';
+
+    // Iterate through selected medication categories
+    for (const category in medications) {
+        const categoryHeader = document.createElement('h2');
+        categoryHeader.textContent = category;
+        medicationsList.appendChild(categoryHeader);
+
+        // Iterate through medications in the category
+        medications[category].forEach(medication => {
+            const listItem = document.createElement('div');
+            listItem.textContent = `${medication.name} - ${medication.dosage} - ${medication.frequency}`;
+            medicationsList.appendChild(listItem);
+        });
+    }
+}
